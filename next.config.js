e70c374b0697f89dabed8f159e272fe17eb4bb72
Omer Ben haroosh <automatic.il129@gmail.com>
2024-11-24 17:54:37 +0200

    Update configuration files and type definitions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // אין צורך בהגדרה מיוחדת עבור src/pages, זה ברירת המחדל ב-Next.js
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
