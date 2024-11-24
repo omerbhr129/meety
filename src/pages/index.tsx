2e6c81827173343e6dd4959c347fd1a3db009960
Omer Ben haroosh <automatic.il129@gmail.com>
2024-11-24 17:48:58 +0200

    Fix TypeScript and ESLint issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,6 +119,7 @@ export default function Home() {
                   ? "text-blue-600 border-b-2 border-blue-600 font-semibold"
                   : "text-gray-500 hover:text-gray-700"
               }`}
+              type="button"
             >
               התחברות
             </button>
@@ -133,6 +134,7 @@ export default function Home() {
                   ? "text-blue-600 border-b-2 border-blue-600 font-semibold"
                   : "text-gray-500 hover:text-gray-700"
               }`}
+              type="button"
             >
               הרשמה
             </button>
